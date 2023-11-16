03abce40e0d4e78500c785dd6321a3f08350c1c4
ldmfabio <ldmfabio@gmail.com>
2023-11-16 07:52:33 -0300

    1INFO3 - 16-11-2023

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
-function clq1() {
-    alert("Você clicou no Botão 1");
-}
-
-let bt2 = document.getElementById("bt2");
-bt2.addEventListener("click", clq2);
-
-function clq2() {
-    alert("Você clicou no Botão 2");
-}
-
 let valor1 = document.querySelector("#valor1");
 let valor2 = document.querySelector("#valor2");
 let resultado = document.querySelector("#resultado");
 
-valor1.addEventListener("keyup", function() {
-    somar(Number(valor1.value), Number(valor2.value));
-});
-
-valor2.addEventListener("keyup", function() {
+let btSoma = document.querySelector("#btSoma");
+btSoma.addEventListener("click", function() {
     somar(Number(valor1.value), Number(valor2.value));
 });
 
@@ -25,3 +11,16 @@ function somar(a, b) {
     let soma = a + b;
     resultado.innerHTML = soma;
 }
+
+let selectedLanguage = document.querySelector("#idioma");
+selectedLanguage.addEventListener("change", function() {
+    if (this.value == "br") {
+        btSoma.value = "Soma";
+    }
+    else if (this.value == "en") {
+        btSoma.value = "Sum";
+    }
+    else if (this.value == "es") {
+        btSoma.value = "Suma";
+    }
+});
